d86b9a6bcb6d89424728a1eb46a71b4a57674525
korenbrand <39384249+korenbrand@users.noreply.github.com>
2018-06-13 14:46:03 +0300

    New file reader

--- AlgoChecker.py
+++ AlgoChecker.py
@@ -4,6 +4,8 @@ import numpy as np
 
 import threading
 import sys
+import time
+
 
 class TimeoutError(Exception): pass
 
@@ -36,58 +38,58 @@ def timelimit(timeout):
 
     return internal
 
+
 class AlgoChecker:
     @staticmethod
-#    @timelimit(266)
-    def checkHeuristic(list_of_hueristics_tuples, num_of_runs, is_advanced, filename):
+    #    @timelimit(266)
+    def check_heuristic(list_of_hueristics_tuples, num_of_runs, is_advanced, filename):
         pass
-        board = Board.Board()
-        board.print_board()
-        counter = 0
+        board = Board.Board(height=9, width=9)
+        # board.print_board()
+        for heuristic_tuple in list_of_hueristics_tuples:
+            if not is_advanced:
+                heuristic_player = HeuristicPlayer(heuristic_tuple)
+            else:
+                heuristic_player = AdvancedHeuristicPlayer(heuristic_tuple)
+            results_data = board.play_game_with_random(heuristic_player, num_of_runs=num_of_runs, detailed=False)
+
         with open(filename + ".txt", "a") as results_file:
             # the name of the file is the name of the first heuristics tuple
-            for heuristic_tuple in list_of_hueristics_tuples:
-                if not is_advanced:
-                    heuristic_player = HeuristicPlayer(heuristic_tuple)
-                else:
-                    heuristic_player = AdvancedHeuristicPlayer(heuristic_tuple)
-                results_data = board.play_game_with_random(heuristic_player, num_of_runs=num_of_runs, detailed=False)
-                if not is_advanced:
-                    results_file.write("S#")
-                else:
-                    results_file.write("H#")
-                results_file.write(str(heuristic_tuple[0]))
-                for heuristica in heuristic_tuple[1:]:
-                    results_file.write("," + str(heuristica))
-                results_file.write("#")
-                results_file.write(str(results_data[0]))
-                for result in results_data[1:]:
-                    results_file.write("," + str(result))
-                results_file.write("\n")
-
-
+            if not is_advanced:
+                results_file.write("S#")
+            else:
+                results_file.write("H#")
+            results_file.write(str(heuristic_tuple[0]))
+            for heuristica in heuristic_tuple[1:]:
+                results_file.write("," + str(heuristica))
+            results_file.write("#")
+            results_file.write(str(results_data[0]))
+            for result in results_data[1:]:
+                results_file.write("," + str(result))
+            results_file.write("\n")
 
 
 # for i in range(10,110,10) :
 #    a = open("C:/Users/t8374100/Desktop/results for different hurestics/convergence2.txt", "a")
 #    a.write(str(i) + ": ")
 #    a.close()
-#    AlgoChecker.checkHeuristic([(1, 2, 3, 4, 1)], i, False, "convergence2")
+#    AlgoChecker.check_heuristic([(1, 2, 3, 4, 1)], i, False, "convergence2")
 # for i in range(100,1100,100) :
 #    a = open("C:/Users/t8374100/Desktop/results for different hurestics/convergence2.txt", "a")
 #    a.write(str(i) + ": ")
 #    a.close()
-#    AlgoChecker.checkHeuristic([(1, 2, 3, 4, 1)], i, False, "convergence2")
+#    AlgoChecker.check_heuristic([(1, 2, 3, 4, 1)], i, False, "convergence2")
 # for i in range (4000, 5500, 500):
 #    a = open("C:/Users/t8374100/Desktop/results for different hurestics/convergence2.txt", "a")
 #    a.write(str(i) + ": ")
 #    a.close()
-AlgoChecker.checkHeuristic([(0,0,0,0,0)],2500, False,"[0,0,0,0,0]")
+"""
+AlgoChecker.check_heuristic([(0,0,0,0,0)],2500, False,"[0,0,0,0,0]")
 for i in range(1, 7, 1):
     for j in range(1, 7, 1):
         for k in range(1, 7, 1):
             try:
-                AlgoChecker.checkHeuristic([(0.01, i, j, k, 0)], 2500, False, "checker")
+                AlgoChecker.check_heuristic([(0.01, i, j, k, 0)], 2500, False, "checker")
                 break
             except:
                 print ("timeout")
@@ -99,12 +101,31 @@ for a in range (2):
         for c in range(2):
             for d in range (2):
                 for e in range(2):
-                    AlgoChecker.checkHeuristic([(a, b, c, d, e)], 2500, False, "BinaryCheck")
+                    AlgoChecker.check_heuristic([(a, b, c, d, e)], 2500, False, "BinaryCheck")
+"""
 
+output_dir = r"C:/Users/Koren/Documents/SMOP/"
+test_night_one_stop = (1, 2.5, 8.5, 7, 0)
+for x in np.arange(-3, 5, 0.5):
+            start = time.time()
+            test_name = "Stripe variance check"
+            print test_name
+            AlgoChecker.check_heuristic([(1, 3 + x, 7, 7, 0)], 2500, False, output_dir + test_name)
+            end = time.time()
+            print 'time: ' + str(end - start)
 
-output_dir = "C:\\Users\\Koren\\Documents\\SMOP\\"
+for x in np.arange(-5, 5, 0.5):
+            start = time.time()
+            test_name = "Wrap variance check"
+            print test_name
+            AlgoChecker.check_heuristic([(1, 3, 7 + x, 7, 0)], 2500, False, output_dir + test_name)
+            end = time.time()
+            print 'time: ' + str(end - start)
 
-for x in np.arange(0, 2.01, 0.05):
-    print "value: " + str(x)
-    AlgoChecker.checkHeuristic([(0.01, 1, 2, 3, 0)], 2500, False, output_dir + "check score calibration")
-
+for x in np.arange(-5, 5, 0.5):
+            start = time.time()
+            test_name = "Chocolate variance check"
+            print test_name
+            AlgoChecker.check_heuristic([(1, 3, 7, 7 + x, 0)], 2500, False, output_dir + test_name)
+            end = time.time()
+            print 'time: ' + str(end - start)
